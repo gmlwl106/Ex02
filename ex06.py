5e9d8e4418c531473b7097c8d2afd9fdbca2303a
gmlwl106 <66778894@naver.com>
2022-07-19 10:08:09 +0900

    파이썬

--- ex06.py
+++ ex06.py
@@ -12,3 +12,17 @@ print("orange", "banana", "apple")
 print("orange", "banana", "apple", sep=",")
 print("orange", "banana", "apple", end="======")
 print("orange", "banana", "apple", end="\n")
+print("orange", "banana", "apple", sep=" ", end="\n")
+print("orange", "banana", "apple", sep=" ", end="=====")
+
+print("====================================================")
+"""
+print("숫자를 입력해주세요")
+no = input()
+print(no)
+"""
+
+print("숫자를 입력해주세요")
+print("숫자:", end="")
+num = input()
+print(num)
